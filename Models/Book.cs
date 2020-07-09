e85a705845a6460477c1a02ef96e601f72f826cc
Ocraniawan Patattan <ocraniawanpatattan@gmail.com>
2020-07-09 12:02:06 +0700

    gitignore and models
    adding gitignore and models

---
+++ Models/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListofPOC.Models
+{
+    public partial class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime? IsNew { get; set; }
+        public DateTime? IsUpdate { get; set; }
+    }
+}
---
+++ Models/ListofPOCContext.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ListofPOC.Models
+{
+    public partial class ListofPOCContext : DbContext
+    {
+        public ListofPOCContext()
+        {
+        }
+
+        public ListofPOCContext(DbContextOptions<ListofPOCContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Book> Book { get; set; }
+        public virtual DbSet<Student> Student { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("server=LAPTOP-KPTB5QRE\\SQLEXPRESS;database=ListofPOC;uid=sa;password=password@123;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.Property(e => e.Description)
+                    .HasMaxLength(100)
+                    .IsFixedLength();
+
+                entity.Property(e => e.IsNew)
+                    .HasColumnName("isNew")
+                    .HasColumnType("datetime");
+
+                entity.Property(e => e.IsUpdate)
+                    .HasColumnName("isUpdate")
+                    .HasColumnType("datetime");
+
+                entity.Property(e => e.Title)
+                    .HasMaxLength(20)
+                    .IsFixedLength();
+            });
+
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.Property(e => e.Class)
+                    .HasMaxLength(5)
+                    .IsFixedLength();
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .IsFixedLength();
+
+                entity.Property(e => e.Status)
+                    .IsRequired()
+                    .HasMaxLength(1)
+                    .IsFixedLength();
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
